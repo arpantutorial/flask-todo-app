82b4d2f30a943f6559f5655aeb96b9567973e843
Arpan tiwari <arpan14m@gmail.com>
2026-09-13 15:10:07 +0530

    Initial commit - Flask Todo application

---
+++ app.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Database Model
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+# Home Route: Add & View Todos
+@app.route('/', methods=['GET', 'POST'])
+# Home Route: Add, View & Search Todos
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        if title.strip() != "":
+            todo = Todo(title=title, desc=desc)
+            db.session.add(todo)
+            db.session.commit()
+            return redirect('/')
+
+    # Check for search query (?search=...)
+    search_query = request.args.get('search')
+    if search_query:
+        allTodo = Todo.query.filter(
+            (Todo.title.contains(search_query)) | (Todo.desc.contains(search_query))
+        ).all()
+    else:
+        allTodo = Todo.query.all()
+
+    return render_template('index.html', allTodo=allTodo)
+
+# Update Route (Added)
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect('/')
+        
+    return render_template('update.html', todo=todo)
+
+# Delete Route
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
