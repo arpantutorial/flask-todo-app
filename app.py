5176cd12a659aff73299a258da11f989b0a376e2
Arpan tiwari <arpan14m@gmail.com>
2026-09-13 16:37:26 +0530

    Fix database table creation for production

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from datetime import datetime
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
 
-
-# Database Model
+# Todo Database Model
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -20,53 +17,44 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-# Home Route: Add & View Todos
-@app.route('/', methods=['GET', 'POST'])
-# Home Route: Add, View & Search Todos
+# Auto-create tables on startup (works for both local and Gunicorn/Render)
+with app.app_context():
+    db.create_all()
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        if title.strip() != "":
-            todo = Todo(title=title, desc=desc)
-            db.session.add(todo)
-            db.session.commit()
-            return redirect('/')
-
-    # Check for search query (?search=...)
-    search_query = request.args.get('search')
-    if search_query:
-        allTodo = Todo.query.filter(
-            (Todo.title.contains(search_query)) | (Todo.desc.contains(search_query))
-        ).all()
-    else:
-        allTodo = Todo.query.all()
-
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        
+    allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
 
-# Update Route (Added)
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
     if request.method == 'POST':
-        todo.title = request.form['title']
-        todo.desc = request.form['desc']
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
         db.session.commit()
-        return redirect('/')
+        return redirect("/")
         
+    todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
-# Delete Route
 @app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
-    if todo:
-        db.session.delete(todo)
-        db.session.commit()
-    return redirect('/')
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
