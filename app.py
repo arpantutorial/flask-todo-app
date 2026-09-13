9be28c893c357a72320362f5113dd651b8f77119
Arpan tiwari <arpan14m@gmail.com>
2026-09-13 16:44:51 +0530

    Revamp UI theme and add task priority feature

--- app.py
+++ app.py
@@ -3,56 +3,54 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+# Using a new db name to automatically create the updated schema with 'priority'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///taskflow.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Todo Database Model
-class Todo(db.Model):
+class Task(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
+    priority = db.Column(db.String(20), default="Medium")
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-# Auto-create tables on startup (works for both local and Gunicorn/Render)
 with app.app_context():
     db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def home():
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
-        db.session.add(todo)
+        priority = request.form.get('priority', 'Medium')
+        new_task = Task(title=title, desc=desc, priority=priority)
+        db.session.add(new_task)
         db.session.commit()
         return redirect("/")
         
-    allTodo = Todo.query.all()
-    return render_template('index.html', allTodo=allTodo)
+    all_tasks = Task.query.order_by(Task.date_created.desc()).all()
+    return render_template('index.html', all_tasks=all_tasks)
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
+    task = Task.query.filter_by(sno=sno).first_or_404()
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title = title
-        todo.desc = desc
-        db.session.add(todo)
+        task.title = request.form['title']
+        task.desc = request.form['desc']
+        task.priority = request.form.get('priority', 'Medium')
         db.session.commit()
         return redirect("/")
         
-    todo = Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html', todo=todo)
+    return render_template('update.html', task=task)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
+    task = Task.query.filter_by(sno=sno).first_or_404()
+    db.session.delete(task)
     db.session.commit()
     return redirect("/")
 
