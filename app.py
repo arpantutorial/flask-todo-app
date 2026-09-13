78117953d207d5120100d7a0b40235e5d5848d63
Arpan tiwari <arpan14m@gmail.com>
2026-09-13 16:27:13 +0530

    Auto-create database tables on startup

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()git add app.py
+
 
 # Database Model
 class Todo(db.Model):
